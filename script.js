47bcaf8bc5f5d8034d7e444324a8de93dbc7a701
pmayur <thisispatil@gmail.com>
2020-12-02 18:03:36 +0530

    Add blank svg canvas to the map

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(d3)
+const MOVIE_SALES =
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    width = 600 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3
+    .select("#root")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
